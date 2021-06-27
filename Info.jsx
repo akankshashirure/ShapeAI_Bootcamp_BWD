a26130b2fe5f31fb8782bef16ed0ab1baec45c78
akankshashirure <85221939+akankshashirure@users.noreply.github.com>
2021-06-27 14:34:52 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>A basic Web dev bootcamp on Js Bootcamp</p>
+      </div>
+      <div className="note">
+        <h1>Bootcamp on BOOTSTRAP</h1>
+        <p>A basic Web deveopment bootcamp on Bootstrap form Scratch.</p>
+      </div>
+      <div className="note">
+        <h1>Bootcamp on Excel </h1>
+        <p>A basic Web deveopment bootcamp on EXcel form Scratch.</p>
+      </div>
+      <div className="note">
+        <h1>Bootcamp on PYTHON</h1>
+        <p>A basic Web deveopment bootcamp on PYTHON form Scratch.</p>
+      </div>
+      <div className="note">
+        <h1>Bootcamp on Network Security</h1>
+        <p>A basic Web deveopment bootcamp on Network security form Scratch.</p>
+      </div>
+      <div className="note">
+        <h1>Bootcamp on IOT</h1>
+        <p>A basic Web deveopment bootcamp on IOT form Scratch.</p>
+      </div>
+    </div>
+  );
+}
+export default Info;
